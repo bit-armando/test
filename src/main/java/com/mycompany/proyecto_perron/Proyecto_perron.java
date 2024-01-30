77c2a4fb9c89309df096708590a81168161bc678
Armando Rosales <refill.1509@gmail.com>
2024-01-29 18:36:10 -0700

    first commit

---
+++ src/main/java/com/mycompany/proyecto_perron/Proyecto_perron.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyecto_perron;
+
+public class Proyecto_perron {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/proyecto_perron/encapsulamiento.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyecto_perron;
+
+public class encapsulamiento {
+    public String Set_text(){
+        String text = "Hola desde la funcion";
+        return text;
+    }
+}
